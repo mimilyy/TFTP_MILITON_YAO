b5c859a92a2dc9ceca692c227605857f974dc724
PierreMiliton <165790642+PierreMiliton@users.noreply.github.com>
2024-12-17 08:15:26 +0100

    question 4a

--- Question4a.c
+++ Question4a.c
@@ -1,4 +1,4 @@
-//Question 4a (unfinished)
+//Question 4a 
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -11,15 +11,67 @@
 
 #define TFTP_OPCODE_RRQ 1
 
-// Structure for the RRQ 
+//Structure for the RRQ 
 struct TFTPRequestPacket {
     uint16_t opcode;   
     char filename[512]; 
     char mode[10];      
 };
 
-void gettftp(char *serveurhost,char *filename){
-    
+//Function to send a RRQ packet to the server
+void sendRRQ(int sockfd, struct sockaddr *server_addr, const char *filename) {
+    struct TFTPRequestPacket rrqPacket;
+
+    //Construct the RRQ packet
+    rrqPacket.opcode = htons(TFTP_OPCODE_RRQ);
+    strncpy(rrqPacket.filename, filename, sizeof(rrqPacket.filename));
+    strncpy(rrqPacket.mode, "octet", sizeof(rrqPacket.mode));
+
+    //Send the RRQ packet to the server
+    sendto(sockfd, &rrqPacket, sizeof(rrqPacket), 0, server_addr, sizeof(struct sockaddr));
+}
+
+
+//Main function for the gettftp program
+void gettftp(char *serverhost, char *filename) {
+    struct addrinfo hints, *res;
+    memset(&hints, 0, sizeof(struct addrinfo));
+    //Use IPv4
+    hints.ai_family = AF_INET;    
+    //Datagram socket for TFTP  
+    hints.ai_socktype = SOCK_DGRAM; 
+
+    int status = getaddrinfo(serverhost, "1069", &hints, &res); 
+    if (status != 0) {
+        fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
+        exit(EXIT_FAILURE);
+    }
+
+    //Extract the address of the server
+    struct sockaddr_in *ipv4 = (struct sockaddr_in *)res->ai_addr;
+    char ip_str[INET_ADDRSTRLEN];
+    inet_ntop(AF_INET, &(ipv4->sin_addr), ip_str, INET_ADDRSTRLEN);
+
+    //Create a socket and verify if it has been created
+    int sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+    if (sockfd == -1) {
+        perror("Socket creation error");
+        exit(EXIT_FAILURE);
+    }
+
+    //Connect to the server
+    if (connect(sockfd, res->ai_addr, res->ai_addrlen) == -1) {
+        perror("Connection error with the server");
+        close(sockfd);
+        exit(EXIT_FAILURE);
+    }
+
+    //Send RRQ to the server
+    sendRRQ(sockfd, (struct sockaddr *)ipv4, filename);
+    //Free the linked list when done with it
+    freeaddrinfo(res); 
+    //Close the socket when done
+    close(sockfd);     
 }
 
 void puttftp(char *serveur, char *filename){
@@ -36,45 +88,8 @@ int main(int argc,char *argv[]){
     char *serverhost = argv[1];
     char *filename = argv[2];
 
-    struct addrinfo hints,*res;
-    memset(&hints, 0, sizeof(struct addrinfo));
-    //IPv4
-    hints.ai_family = AF_INET;      
-    //Datagram socket for TFTP
-    hints.ai_socktype = SOCK_DGRAM; 
-
-    int status = getaddrinfo(serverhost, "69", &hints, &res);
-    if (status != 0) {
-        fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
-        exit(EXIT_FAILURE);
-    }
-
-    //Extract the address of the server
-    struct sockaddr_in *ipv4 = (struct sockaddr_in *)res->ai_addr;
-    char ip_str[INET_ADDRSTRLEN];
-    inet_ntop(AF_INET, &(ipv4->sin_addr), ip_str, INET_ADDRSTRLEN);
-    //Print the IP address
-    printf("Server IP: %s\n", ip_str); 
-    
-    //Create a socket and verify if it has been created
-    int sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-    if (sockfd == -1) {
-        perror("Socket creation error");
-        exit(EXIT_FAILURE);
-    }
-
-    //Connect to the server
-    if (connect(sockfd, res->ai_addr, res->ai_addrlen) == -1) {
-        perror("Connection error with the server");
-        close(sockfd);
-        exit(EXIT_FAILURE);
-    }
-
-    //Free the memory allocated
-    freeaddrinfo(res); 
-
-    puttftp(serverhost, filename);
-    //gettftp(serverhost,filename);
+    //puttftp(serverhost, filename);
+    gettftp(serverhost,filename);
 
     return EXIT_SUCCESS;
 }
